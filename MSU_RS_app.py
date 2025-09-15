b25e6080999aba631f6922910d52a4f5506948e3
Mana Kashuk <140280356+ManaKashuk@users.noreply.github.com>
2025-09-14 19:34:41 -0500

    Update MSU_RS_app.py

--- MSU_RS_app.py
+++ MSU_RS_app.py
@@ -50,6 +50,7 @@ st.markdown(
     f"""
     <div style='text-align:left;'>
         <img src='data:image/png;base64,{logo_base64}' width='700'/>
+        <h2>MSU Research Security Assistant</h2>
         <h5><i>🧠 Smart Assistant for Research Integrity, Compliance & Security Support</i></h5>
         <p>🛡️ Trained on MSU Office of Research Administration Internal SOPs and Federal Guidance.</p>
     </div>
